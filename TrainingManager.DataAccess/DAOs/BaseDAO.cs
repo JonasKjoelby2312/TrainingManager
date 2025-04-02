a4a34359f646c8d3fa77f7242cf6e4d1da997e7e
malt3077 <10503077@ucn.dk>
2025-04-02 10:10:07 +0200

    BaseDAO, Folders + nuget dapper & sqlclient

---
+++ TrainingManager.DataAccess/DAOs/BaseDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingManager.DataAccess.DAOs;
+
+public class BaseDAO
+{
+
+    private string _connectionstring;
+
+    protected BaseDAO(string connectionstring) => _connectionstring = connectionstring;
+
+    protected IDbConnection CreateConnection() => new SqlConnection(_connectionstring);
+}
