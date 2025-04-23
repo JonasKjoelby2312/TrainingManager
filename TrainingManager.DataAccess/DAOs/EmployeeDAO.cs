c13846e8fbfe3e5b0d71f31b52414a986f69f0a1
malt3077 <10503077@ucn.dk>
2025-04-23 11:13:40 +0200

    employeeDAO CreateAsync method made

--- TrainingManager.DataAccess/DAOs/EmployeeDAO.cs
+++ TrainingManager.DataAccess/DAOs/EmployeeDAO.cs
@@ -1,6 +1,8 @@
 ﻿using Dapper;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +15,39 @@ public class EmployeeDAO : BaseDAO, IEmployeeDAO
     private readonly string GET_ALL_EMPLOYEES = "SELECT \r\n    e.employee_id AS EmployeeId,\r\n    e.employee_initials AS Initials,\r\n    e.employee_email AS Email,\r\n    e.employee_is_active AS IsActive,\r\n    STRING_AGG(tr.role_name, ', ') AS RolesAsString  -- Aggregates roles per employee\r\nFROM employees e\r\nLEFT JOIN employee_roles er ON er.fk_employee_id = e.employee_id\r\nLEFT JOIN treat_roles tr ON tr.role_id = er.fk_role_id\r\nGROUP BY \r\n    e.employee_id, e.employee_initials, e.employee_email, e.employee_is_active\r\nORDER BY e.employee_id;";
     private readonly string GET_TRAINING_STATUS = "WITH selected_employee AS (\r\n    SELECT employee_id, employee_initials \r\n    FROM employees \r\n    WHERE employee_initials = @Initials\r\n), \r\nactive_revisions AS (\r\n    SELECT \r\n        r.fk_treat_procedure_id, \r\n        r.revision_id,\r\n        ROW_NUMBER() OVER (PARTITION BY r.fk_treat_procedure_id ORDER BY r.revision_id DESC) AS rn\r\n    FROM revisions r\r\n    WHERE r.revision_is_active = 1\r\n), \r\nactive_procedures AS (\r\n    SELECT tp.procedure_id, tp.procedure_name\r\n    FROM treat_procedures tp\r\n    JOIN active_revisions ar ON tp.procedure_id = ar.fk_treat_procedure_id\r\n    WHERE ar.rn = 1\r\n), \r\nemployee_roles_expanded AS (\r\n    SELECT er.fk_role_id \r\n    FROM selected_employee se \r\n    JOIN employee_roles er ON se.employee_id = er.fk_employee_id\r\n), \r\nmax_required_type AS (\r\n    SELECT\r\n        rtt.fk_treat_procedure_id,\r\n        rtt.fk_role_id,\r\n        MAX(rtt.required_type) AS max_required_type\r\n    FROM\r\n        required_training_types rtt\r\n    JOIN employee_roles er ON er.fk_role_id = rtt.fk_role_id\r\n    WHERE er.fk_employee_id = (SELECT employee_id FROM selected_employee)\r\n    GROUP BY\r\n        rtt.fk_treat_procedure_id, rtt.fk_role_id\r\n),\r\ntraining_status_base AS (\r\n    SELECT \r\n        ap.procedure_name, \r\n        MAX(CASE \r\n                WHEN t.ticket_id IS NOT NULL AND r.revision_is_active = 1 THEN 3 \r\n                WHEN mrt.max_required_type = 2 THEN 2\r\n                WHEN mrt.max_required_type = 1 THEN 1\r\n                ELSE 0\r\n            END) AS StatusRank\r\n    FROM \r\n        active_procedures ap\r\n    CROSS JOIN \r\n        selected_employee se \r\n    LEFT JOIN \r\n        employee_roles er ON er.fk_employee_id = se.employee_id \r\n    LEFT JOIN \r\n        max_required_type mrt \r\n        ON mrt.fk_treat_procedure_id = ap.procedure_id \r\n        AND mrt.fk_role_id = er.fk_role_id\r\n    LEFT JOIN \r\n        tickets t ON t.fk_employee_id = se.employee_id \r\n        AND t.fk_treat_procedure_id = ap.procedure_id\r\n    LEFT JOIN \r\n        revisions r ON r.revision_id = t.fk_revision_id\r\n        AND r.revision_is_active = 1\r\n    GROUP BY \r\n        ap.procedure_name\r\n)\r\nSELECT \r\n    procedure_name, \r\n    CASE StatusRank \r\n        WHEN 3 THEN 'Completed' \r\n        WHEN 2 THEN 'Missing' \r\n        WHEN 1 THEN 'If Performing' \r\n        ELSE 'Optional' \r\n    END AS TrainingStatus\r\nFROM \r\n    training_status_base\r\nORDER BY \r\n    procedure_name;";
     private readonly string GET_EMPLOYEE_COMPLIANCE_BY_INITIALS = "WITH selected_employee AS (\r\n    SELECT employee_id, employee_initials \r\n    FROM employees \r\n    WHERE employee_initials = @Initials\r\n), \r\nactive_revisions AS (\r\n    SELECT \r\n        r.fk_treat_procedure_id, \r\n        r.revision_id,\r\n        r.revision,\r\n        ROW_NUMBER() OVER (PARTITION BY r.fk_treat_procedure_id ORDER BY r.revision_id DESC) AS rn\r\n    FROM revisions r\r\n    WHERE r.revision_is_active = 1\r\n), \r\nactive_procedures AS (\r\n    SELECT tp.procedure_id, tp.procedure_name\r\n    FROM treat_procedures tp\r\n    JOIN active_revisions ar ON tp.procedure_id = ar.fk_treat_procedure_id\r\n    WHERE ar.rn = 1\r\n), \r\nemployee_roles_expanded AS (\r\n    SELECT er.fk_role_id \r\n    FROM selected_employee se \r\n    JOIN employee_roles er ON se.employee_id = er.fk_employee_id\r\n), \r\nmax_required_type AS (\r\n    SELECT\r\n        rtt.fk_treat_procedure_id,\r\n        rtt.fk_role_id,\r\n        MAX(rtt.required_type) AS max_required_type\r\n    FROM\r\n        required_training_types rtt\r\n    JOIN employee_roles er ON er.fk_role_id = rtt.fk_role_id\r\n    WHERE er.fk_employee_id = (SELECT employee_id FROM selected_employee)\r\n    GROUP BY\r\n        rtt.fk_treat_procedure_id, rtt.fk_role_id\r\n),\r\ntraining_status_base AS (\r\n    SELECT \r\n        ap.procedure_name, \r\n        MAX(CASE \r\n                WHEN t.ticket_id IS NOT NULL AND r.revision_is_active = 1 THEN 3 \r\n                WHEN mrt.max_required_type = 2 THEN 2\r\n                WHEN mrt.max_required_type = 1 THEN 1\r\n                ELSE 0\r\n            END) AS StatusRank,\r\n        r.revision AS completed_revision,\r\n        ar.revision AS required_revision\r\n    FROM \r\n        active_procedures ap\r\n    CROSS JOIN \r\n        selected_employee se \r\n    LEFT JOIN \r\n        employee_roles er ON er.fk_employee_id = se.employee_id \r\n    LEFT JOIN \r\n        max_required_type mrt \r\n        ON mrt.fk_treat_procedure_id = ap.procedure_id \r\n        AND mrt.fk_role_id = er.fk_role_id\r\n    LEFT JOIN \r\n        tickets t ON t.fk_employee_id = se.employee_id \r\n        AND t.fk_treat_procedure_id = ap.procedure_id\r\n    LEFT JOIN \r\n        revisions r ON r.revision_id = t.fk_revision_id\r\n        AND r.revision_is_active = 1\r\n    LEFT JOIN\r\n        active_revisions ar ON ar.fk_treat_procedure_id = ap.procedure_id AND ar.rn = 1\r\n    GROUP BY \r\n        ap.procedure_name, r.revision, ar.revision\r\n)\r\nSELECT \r\n    procedure_name, \r\n    CASE StatusRank \r\n        WHEN 3 THEN 'Completed' \r\n        WHEN 2 THEN 'Missing' \r\n        WHEN 1 THEN 'If Performing' \r\n        ELSE 'Optional' \r\n    END AS TrainingStatus,\r\n    completed_revision,\r\n    required_revision\r\nFROM \r\n    training_status_base\r\nORDER BY \r\n    procedure_name;";
+    private readonly string INSERT_EMPLOYEE = "INSERT INTO employees (employee_email, employee_initials, employee_is_active) Values (@Email, @Initials, @IsActive) SELECT CAST(SCOPE_IDENTITY() as INT);";
+    private readonly string INSERT_EMPLOYEE_ROLES = "INSERT INTO employee_roles (fk_employee_id, fk_role_id) VALUES (@EmployeeId, @RoleId)";
+    private readonly string GET_ROLES_IDS_BY_ROLE_NAMES = "SELECT role_id FROM treat_roles WHERE role_name IN @Roles"; //Marked
 
     public EmployeeDAO(string connectionString) : base(connectionString)
     {
     }
 
-    public Task<int> CreateAsync(Employee entity)
+    public async Task<int> CreateAsync(Employee entity)
     {
-        throw new NotImplementedException();
+        using var connection = CreateConnection();
+        connection.Open();
+        IDbTransaction transaction = connection.BeginTransaction();
+        try
+        {
+            int IdFromDB = await connection.ExecuteScalarAsync<int>(INSERT_EMPLOYEE, new { Email = entity.Email, Initials = entity.Initials, IsActive = entity.IsActive });
+
+            IEnumerable<int> rolesIds = await connection.QueryAsync<int>(GET_ROLES_IDS_BY_ROLE_NAMES, new { Roles = entity.Roles });
+
+            foreach (int roleId in rolesIds)
+            {
+                await connection.QueryAsync(INSERT_EMPLOYEE_ROLES, new { EmployeeId = IdFromDB, RoleId = roleId });
+            }
+
+            transaction.Commit();
+
+            return IdFromDB;
+        }
+        catch (Exception ex)
+        {
+            transaction.Rollback();
+            throw new Exception($"Could not create employee with initials: {entity.Initials}, message was: {ex.Message}", ex);
+        }
     }
 
     public async Task<IEnumerable<Employee>> GetAllEmployeesAndStatusesAsync()
