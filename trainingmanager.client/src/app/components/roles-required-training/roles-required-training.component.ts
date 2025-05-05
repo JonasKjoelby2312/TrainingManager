8b1481179705c93f590809365d11e5b5920243f0
JonasKjoelby2312 <150658707+JonasKjoelby2312@users.noreply.github.com>
2025-05-05 09:46:13 +0200

    Create new proceudre, in angular. HTLM, CSS AND TS.

--- trainingmanager.client/src/app/components/roles-required-training/roles-required-training.component.ts
+++ trainingmanager.client/src/app/components/roles-required-training/roles-required-training.component.ts
@@ -6,7 +6,7 @@ interface RolesRequiredTraining {
   roleName: string;
   trainingRequiredTypes: {
     [procedureName: string]: number;
-  }
+  };
 }
 
 @Component({
@@ -17,23 +17,60 @@ interface RolesRequiredTraining {
 })
 export class RolesRequiredTrainingComponent {
   rolesRequiredTraining: RolesRequiredTraining[] = [];
-  //roleNames: string[] = [];
-  //requiredTypes: number[] = [];
   procedures: string[] = [];
+  isCreateProcedureActive: boolean = false;
+
+  // For procedure input and training selections
+  procedureInputValue: string = '';
+  trainingSelections: { [roleId: number]: number } = {};
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<RolesRequiredTraining[]>('https://localhost:7227/api/RolesRequiredTraining').subscribe(data => {
-      this.rolesRequiredTraining = data; console.log(data)
+    this.http.get<RolesRequiredTraining[]>('https://localhost:7227/api/RolesRequiredTraining').subscribe(
+      data => {
+        this.rolesRequiredTraining = data;
+        console.log(data);
 
-      const procedureSet = new Set<string>();
-      data.forEach(role => {
-        Object.keys(role.trainingRequiredTypes).forEach(proc => procedureSet.add(proc));
-      });
+        const procedureSet = new Set<string>();
+        data.forEach(role => {
+          Object.keys(role.trainingRequiredTypes).forEach(proc => procedureSet.add(proc));
+        });
 
-      this.procedures = Array.from(procedureSet).sort();
+        this.procedures = Array.from(procedureSet).sort();
+      },
+      error => console.log(error)
+    );
+  }
 
-    }, error => console.log(error));
+  // Show modal
+  openCreateProcedure() {
+    this.isCreateProcedureActive = true;
+    console.log('openCreateProcedure called');
+    console.log(this.isCreateProcedureActive);
+  }
+
+  // Cancel modal
+  onCancelProcedure() {
+    this.isCreateProcedureActive = false;
+    this.trainingSelections = {};
+    this.procedureInputValue = '';
+  }
+
+  // Handle radio button change
+  setRoleSelection(roleId: number, value: number) {
+    this.trainingSelections[roleId] = value;
+  }
+
+  // Handle create
+  onCreateProcedure(procedureName: string) {
+    console.log('Creating Procedure:', procedureName);
+    console.log('Selected Role Trainings:', this.trainingSelections);
+
+    // Add POST logic here to send data to the API if needed
+
+    this.isCreateProcedureActive = false;
+    this.trainingSelections = {};
+    this.procedureInputValue = '';
   }
 }
